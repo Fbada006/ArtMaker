b159846e2867c65d42360017be05aef63f32910a
CalebK <langat.caleb25@gmail.com>
2024-07-23 22:20:07 +0300

    fix main branch runtime errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation(":artmaker")
+    implementation(project(":artmaker"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
