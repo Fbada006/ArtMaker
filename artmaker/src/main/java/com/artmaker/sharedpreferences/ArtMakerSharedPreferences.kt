071d760ad26ef5a037674eeb461d01cfdfb10fbd
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-07-25 10:38:55 +0300

    Implement the set() and get() functions...

--- artmaker/src/main/java/com/artmaker/sharedpreferences/ArtMakerSharedPreferences.kt
+++ artmaker/src/main/java/com/artmaker/sharedpreferences/ArtMakerSharedPreferences.kt
@@ -12,23 +12,35 @@ internal class ArtMakerSharedPreferences<T>(
 ) {
 
     private val artMakerSharedPreferences = context.getSharedPreferences(
-        "artMakerSharedPrefernces",
+        "artMakerSharedPreferences",
         Context.MODE_PRIVATE
     )
 
-    inline fun <reified T> SharedPreferences.set(key: String, value: T): T {
+    inline fun <reified T> SharedPreferences.set(key: String, value: T) {
         val editor = this.edit()
         when (T::class) {
             Boolean::class -> editor.putBoolean(key, value as Boolean)
+            Int::class -> editor.putInt(key, value as Int)
+            String::class -> editor.putString(key, value as String)
+            Float::class -> editor.putFloat(key, value as Float)
+            Long::class -> editor.putLong(key, value as Long)
+            else -> {
+                val otherValue = value.toString()
+                artMakerSharedPreferences.edit().putString(key, otherValue).apply()
+            }
         }
         editor.apply()
     }
 
-    inline fun <reified T> SharedPreferences.get(key: String, value: T): T {
+    inline fun <reified T> SharedPreferences.get(key: String, defaultValue: T): T {
         when(T::class) {
-            Boolean::class -> return this.getBoolean(key, value as Boolean) as T
+            Boolean::class -> return this.getBoolean(key, defaultValue as Boolean) as T
+            Int::class -> return this.getInt(key, defaultValue as Int) as T
+            String::class -> return this.getString(key, defaultValue as String) as T
+            Float::class -> return this.getFloat(key, defaultValue as Float) as T
+            Long::class -> return this.getLong(key, defaultValue as Long) as T
         }
-        return value
+        return defaultValue
     }
 
 }
