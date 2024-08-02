1a9ce09d477f37bb91ff8d88aba66358165d5473
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-08-02 15:37:30 +0300

    Optimise imports...

--- artmaker/src/main/java/com/artmaker/composables/ArtMakerStrokeWidthPopup.kt
+++ artmaker/src/main/java/com/artmaker/composables/ArtMakerStrokeWidthPopup.kt
@@ -7,15 +7,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupProperties
 
 @Composable
 internal fun ArtMakerStrokeWidthPopup() {
