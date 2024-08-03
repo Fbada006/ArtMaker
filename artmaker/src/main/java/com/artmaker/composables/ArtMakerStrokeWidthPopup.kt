ef394c60222eb3380c41e4ffbd568a2aca7706ac
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-08-03 09:37:43 +0300

    Try out the AlertDialog composable to act as the ArtMakerStrokeWidthPopup and realise that it will not work due to its lack of the Content (lambda) parameter that would have accommodated the ArtMakerStrokeWidthSlider...

--- artmaker/src/main/java/com/artmaker/composables/ArtMakerStrokeWidthPopup.kt
+++ artmaker/src/main/java/com/artmaker/composables/ArtMakerStrokeWidthPopup.kt
@@ -1,9 +1,11 @@
 package com.artmaker.composables
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
