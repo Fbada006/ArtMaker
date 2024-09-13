cc3a255f668ebf2a1c411ec25a9a992efb9dc1cd
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-09-13 11:26:34 +0300

    Format the code...

--- artmaker/src/main/java/io/artmaker/data/PreferencesManager.kt
+++ artmaker/src/main/java/io/artmaker/data/PreferencesManager.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 ArtMaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.artmaker.data
 
 import androidx.compose.ui.graphics.Color
@@ -10,34 +25,32 @@ import io.artmaker.ArtMakerUIState
 
 internal class PreferencesManager(private val preferences: ArtMakerSharedPreferences) {
 
-    fun loadInitialUIState(): ArtMakerUIState {
-        return ArtMakerUIState(
-            strokeColour = preferences.get(
-                key = PreferenceKeys.PREF_SELECTED_STROKE_COLOUR,
-                defaultValue = Color.Red.toArgb(),
-            ),
-            strokeWidth = preferences.get(
-                key = PreferenceKeys.PREF_SELECTED_STROKE_WIDTH,
-                defaultValue = 5,
-            ),
-            shouldUseStylusOnly = preferences.get(
-                key = PreferenceKeys.PREF_USE_STYLUS_ONLY,
-                false,
-            ),
-            shouldDetectPressure = preferences.get(
-                key = PreferenceKeys.PREF_DETECT_PRESSURE,
-                false,
-            ),
-            canShowEnableStylusDialog = preferences.get(
-                key = PreferenceKeys.PREF_SHOW_ENABLE_STYLUS_DIALOG,
-                true,
-            ),
-            canShowDisableStylusDialog = preferences.get(
-                key = PreferenceKeys.PREF_SHOW_DISABLE_STYLUS_DIALOG,
-                true,
-            ),
-        )
-    }
+    fun loadInitialUIState(): ArtMakerUIState = ArtMakerUIState(
+        strokeColour = preferences.get(
+            key = PreferenceKeys.PREF_SELECTED_STROKE_COLOUR,
+            defaultValue = Color.Red.toArgb(),
+        ),
+        strokeWidth = preferences.get(
+            key = PreferenceKeys.PREF_SELECTED_STROKE_WIDTH,
+            defaultValue = 5,
+        ),
+        shouldUseStylusOnly = preferences.get(
+            key = PreferenceKeys.PREF_USE_STYLUS_ONLY,
+            false,
+        ),
+        shouldDetectPressure = preferences.get(
+            key = PreferenceKeys.PREF_DETECT_PRESSURE,
+            false,
+        ),
+        canShowEnableStylusDialog = preferences.get(
+            key = PreferenceKeys.PREF_SHOW_ENABLE_STYLUS_DIALOG,
+            true,
+        ),
+        canShowDisableStylusDialog = preferences.get(
+            key = PreferenceKeys.PREF_SHOW_DISABLE_STYLUS_DIALOG,
+            true,
+        ),
+    )
 
     fun updateStrokeColor(strokeColour: Int) {
         preferences.set(key = PreferenceKeys.PREF_SELECTED_STROKE_COLOUR, value = strokeColour)
@@ -62,5 +75,4 @@ internal class PreferencesManager(private val preferences: ArtMakerSharedPrefere
     fun updateDisableStylusDialog(canShow: Boolean) {
         preferences.set(key = PreferenceKeys.PREF_SHOW_DISABLE_STYLUS_DIALOG, value = canShow)
     }
-
 }
