e8cebdc6a942dfbd71be9896fdbad5069dc807f8
Ferdinand Bada <ferdinandkamata@duck.com>
2024-09-14 09:06:14 +0300

    cleanup

---
+++ customcolorpalette/src/main/java/com/artmaker/customcolorpalette/CustomColorPalette.kt
@@ -0,0 +1,13 @@
+package com.artmaker.customcolorpalette
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CustomColorPalette(modifier: Modifier = Modifier) {
+    Text(
+        text = "Custom Color palette",
+        modifier = modifier,
+    )
+}
