9a5db665f044f155cf8100736adb2c9256526f2b
Ferdinand Bada <ferdinandkamata@duck.com>
2024-09-20 18:03:32 +0300

    ui improvement

--- artmaker/src/main/java/io/artmaker/composables/StrokePreview.kt
+++ artmaker/src/main/java/io/artmaker/composables/StrokePreview.kt
@@ -1,8 +1,25 @@
+/*
+ * Copyright 2024 ArtMaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.artmaker.composables
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,10 +36,13 @@ import kotlin.math.sin
 internal fun StrokePreview(state: StrokeState, modifier: Modifier = Modifier) {
     var wavyLineOffsets by remember { mutableStateOf(listOf<Offset>()) }
 
-    Box(modifier = modifier) {
+    Box(
+        modifier = modifier
+            .background(color = Color(248, 246, 240), shape = RoundedCornerShape(dimensionResource(R.dimen.Padding12))),
+    ) {
         Canvas(
             modifier = Modifier
-                .padding(vertical = dimensionResource(R.dimen.Padding7))
+                .padding(dimensionResource(R.dimen.Padding7))
                 .matchParentSize(),
         ) {
             val canvasWidth = size.width
@@ -42,10 +62,7 @@ internal fun StrokePreview(state: StrokeState, modifier: Modifier = Modifier) {
     }
 }
 
-internal data class StrokeState(
-    val strokeColor: Int,
-    val strokeWidth: Int,
-)
+internal data class StrokeState(val strokeColor: Int, val strokeWidth: Int)
 
 private fun wavyOffsets(canvasWidth: Float, canvasHeight: Float): List<Offset> {
     val waveAmplitude = canvasHeight / 4
