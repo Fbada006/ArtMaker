53d47d35e2fe9e530a2ea38f0976fc5e1ebe80b4
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-09-23 16:21:21 +0300

    Calculate the next GitHub Release version...

--- createGitHubRelease.sh
+++ createGitHubRelease.sh
@@ -6,23 +6,23 @@
 # Get the next release version based on the input...
 get_next_release_version() {
 
-  local current_release_version=$1
-  local release_type=$2
+  local current_github_release_version=$1
+  local github_release_type=$2
 
-  IFS='.' read -r -a version_parts <<< "$current_release_version"
+  IFS='.' read -r -a github_release_version_parts <<< "$current_github_release_version"
 
-  case $release_type in
+  case $github_release_type in
           "Major")
-            version_parts[0]=$((version_parts[0] + 1))
-            version_parts[1]=0
-            version_parts[2]=0
+            github_release_version_parts[0]=$((github_release_version_parts[0] + 1))
+            github_release_version_parts[1]=0
+            github_release_version_parts[2]=0
             ;;
           "Minor")
-            version_parts[1]=$((version_parts[1] + 1))
-            version_parts[2]=0
+            github_release_version_parts[1]=$((github_release_version_parts[1] + 1))
+            github_release_version_parts[2]=0
             ;;
           "Patch")
-            version_parts[2]=$((version_parts[2] + 1))
+            github_release_version_parts[2]=$((github_release_version_parts[2] + 1))
             ;;
           *)
             echo "Invalid release type"
@@ -30,34 +30,38 @@ get_next_release_version() {
             ;;
   esac
 
-  echo "${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+  echo "${github_release_version_parts[0]}.${github_release_version_parts[1]}.${github_release_version_parts[2]}"
 
 }
 
 # Retrieve the current GitHub Release Version from scripts/publish.gradle.kts...
-current_release_version=$(grep 'libVersion =' scripts/publish.gradle.kts | sed 's/[^0-9.]//g')
+current_github_release_version=$(grep 'libVersion =' scripts/publish.gradle.kts | sed 's/[^0-9.]//g')
 
-echo "Current Release Version is: $current_release_version"
+echo "Current Release Version is: $current_github_release_version"
 
 # Get the Release Type as User Input...
 echo "Please select the type of Release:"
 echo "1) Major (Incompatible API Changes)"
 echo "2) Minor (Backward Compatible Functionality)"
-echo "3) Patch (Backward Compatible BUg Fixes)"
-read -rp "Please choose your Release (1/2/3): " release_type_choice
+echo "3) Patch (Backward Compatible Bug Fixes)"
+read -rp "Please choose your Release (1/2/3): " github_release_type_choice
 
-case $release_type_choice in
+case $github_release_type_choice in
     1)
-        release_type="Major"
+        github_release_type="Major"
         ;;
     2)
-        release_type="Minor"
+        github_release_type="Minor"
         ;;
     3)
-        release_type="Patch"
+        github_release_type="Patch"
         ;;
     *)
         echo "Invalid Choice"
         exit 1
         ;;
 esac
+
+# Calculate the next GitHub Release version...
+next_github_release_version=$(get_next_release_version "$current_github_release_version" "$github_release_type")
+echo "The next GitHub Release version will be: $next_github_release_version"
