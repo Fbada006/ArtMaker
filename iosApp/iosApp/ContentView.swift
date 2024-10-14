a36c078f7448e15914aee3cbd6348afe82315d35
CalebK <langat.caleb25@gmail.com>
2024-10-14 13:32:48 +0300

    Setup IOS demo

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,19 @@
+import UIKit
+import SwiftUI
+import shared
+
+
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.mainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct ContentView: View {
+    var body: some View {
+        ComposeView()
+                .ignoresSafeArea(.keyboard)
+    }
+}
