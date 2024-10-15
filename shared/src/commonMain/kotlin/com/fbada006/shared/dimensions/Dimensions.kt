fdc0044a3cae76e4b3e70f2a3fbdcc399a59e963
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-10-15 16:26:10 +0300

    Migrate the Dimensions...

---
+++ shared/src/commonMain/kotlin/com/fbada006/shared/dimensions/Dimensions.kt
@@ -0,0 +1,27 @@
+package com.fbada006.shared.dimensions
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object Dimensions {
+    val Padding0: Dp = 0.dp
+    val Padding2: Dp = 2.dp
+    val Padding4: Dp = 4.dp
+    val Padding7: Dp = 7.dp
+    val Padding8: Dp = 8.dp
+    val Padding10: Dp = 10.dp
+    val Padding12: Dp = 12.dp
+    val Padding30: Dp = 30.dp
+    val Padding32: Dp = 32.dp
+    val Padding48: Dp = 48.dp
+    val Padding60: Dp = 60.dp
+    val ColorPaletteHeight: Dp = 330.dp
+
+    // Line style UI
+    val LineStyleSelectorHeight: Dp = 54.dp
+    val LineStyleOptionWidth: Dp = 54.dp
+    val LineStyleOptionHeight: Dp = 48.dp
+    val LineStyleSelectorSpacing: Dp = 8.dp
+    val LineStyleOptionPadding: Dp = 4.dp
+    val LineStyleOptionCanvas: Dp = 40.dp
+}
