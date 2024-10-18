46d7d2613fdc115793c6e3d93d9bbdabd7e674a7
Ferdinand Bada <ferdinandkamata@duck.com>
2024-10-18 12:33:21 +0300

    fix ui on ios

--- shared/src/iosMain/kotlin/com/fbada006/shared/utils/PermissionUtils.ios.kt
+++ shared/src/iosMain/kotlin/com/fbada006/shared/utils/PermissionUtils.ios.kt
@@ -1,9 +1,11 @@
 package com.fbada006.shared.utils
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.fbada006.shared.actions.ArtMakerAction
 
 @Composable
-actual fun ShareableContent(shouldExport:Boolean,onAction: (ArtMakerAction) -> Unit, content: @Composable () -> Unit, modifier: Modifier) {
+actual fun ShareableContent(shouldExport: Boolean, onAction: (ArtMakerAction) -> Unit, content: @Composable () -> Unit, modifier: Modifier) {
+    Box(modifier) { content() }
 }
