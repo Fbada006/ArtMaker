4a36beb508f36508b29b460b61663f705de36b03
Ferdinand Bada <ferdinandkamata@duck.com>
2024-10-25 13:00:52 +0300

    fix ios build issues

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import io.artmaker
+import artmaker
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
