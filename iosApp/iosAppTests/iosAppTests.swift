f1348796b376d764f2de4f589f76b8659ca6c93d
Ferdinand Bada <ferdinandkamata@duck.com>
2024-11-06 14:31:33 +0300

    test disable

--- iosApp/iosAppTests/iosAppTests.swift
+++ iosApp/iosAppTests/iosAppTests.swift
@@ -5,12 +5,12 @@
 //  Created by Caleb Langat on 14/10/2024.
 //
 
-import Testing
-
-struct iosAppTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
-}
+//import Testing
+//
+//struct iosAppTests {
+//
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
+//
+//}
--- iosApp/iosAppUITests/iosAppUITestsLaunchTests.swift
+++ iosApp/iosAppUITests/iosAppUITestsLaunchTests.swift
@@ -5,29 +5,29 @@
 //  Created by Caleb Langat on 14/10/2024.
 //
 
-import XCTest
-
-final class iosAppUITestsLaunchTests: XCTestCase {
-
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    @MainActor
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
-    }
-}
+//import XCTest
+//
+//final class iosAppUITestsLaunchTests: XCTestCase {
+//
+//    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+//        true
+//    }
+//
+//    override func setUpWithError() throws {
+//        continueAfterFailure = false
+//    }
+//
+//    @MainActor
+//    func testLaunch() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        // Insert steps here to perform after app launch but before taking a screenshot,
+//        // such as logging into a test account or navigating somewhere in the app
+//
+//        let attachment = XCTAttachment(screenshot: app.screenshot())
+//        attachment.name = "Launch Screen"
+//        attachment.lifetime = .keepAlways
+//        add(attachment)
+//    }
+//}
